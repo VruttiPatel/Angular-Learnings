34010861073967965d6ac17f785168e68c23b6b0
VruttiPatel <vruttipatel2686@gmail.com>
2025-09-09 00:27:59 +0530

    Signal set()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['app.component.scss', 'common.component.scss']
 })
 export class AppComponent {
-  writableSignalValue: WritableSignal<number> = signal(100);
+  writableSignalValue = signal(100);
   computedSignalValue = computed(() => 200);
-
-  updateWritableSignal() {
-    this.writableSignalValue.set(this.writableSignalValue() + 1);
-  }
-
-  updateComputedSignal() {
-    this.computedSignalValue;
-    //this.computedSignalValue.set(this.computedSignalValue()+1); - Computed signals are constant so not having set method
-  }
 }
