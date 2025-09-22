f339112161077a2325e0c8b30f12c9897849e31c
VruttiPatel <vruttipatel2686@gmail.com>
2025-09-22 23:10:37 +0530

    *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
+import { NgFor } from '@angular/common';
 import { Component, computed, effect, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, NgFor],
   templateUrl: './app.component.html',
   styleUrls: ['app.component.scss', 'common.component.scss']
 })
 export class AppComponent {
+
+  students: string[] = ['Vrutti', 'Jahanvi', 'Shreya'];
 
 }
